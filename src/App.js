744de32cac00cd83873fe750d18fcb4012487823
kethmipujani <kethmipujani@gmail.com>
2024-06-17 14:46:44 +0530

    maps integrated. bugs in API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import { GoogleMap, useLoadScript } from "@react-google-maps/api";
+
+const libraries = ["places"];
+const mapContainerStyle = {
+  width: '100vw',
+  height: '100vh',
+};
+const center = {
+  lat: 12,
+  lng: 112,
+};
+
+function Map() {
   return (
-    <div className="App">
-      Hardware Project
-    </div>
+    <GoogleMap
+      mapContainerStyle={mapContainerStyle}
+      zoom={10}
+      center={center}
+    />
   );
 }
 
-export default App;
+export default function App() {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: "YOUR_API_KEY_HERE",
+    libraries,
+  });
+
+  if (loadError) return "Error loading maps";
+  if (!isLoaded) return "Loading Maps";
+
+  return (
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <Map />
+    </div>
+  );
+}
